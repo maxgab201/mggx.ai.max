75624e3bced48aa94b65edf647a81f92f78ccc86
maxgab201 <maxgab201@gmail.com>
2026-09-08 16:45:05 -0300

    build: add app module config

---
+++ minecraft-touch-hid/minecrafttouchhid/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.mggx.minecrafttouchhid"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.mggx.minecrafttouchhid"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.0.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildTypes {
+        debug { isMinifyEnabled = false }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    lint {
+        abortOnError = true
+        checkReleaseBuilds = true
+    }
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
